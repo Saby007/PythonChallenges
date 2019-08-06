d15b7ce66b01e84b3bf770c09bc3c11a49957e18
Sabyasachi Samaddar <sabyanew@gmail.com>
2019-08-06 22:13:52 +0530

    Assembled the syllabus

--- pythonChallenges/pythonChallenges.py
+++ pythonChallenges/pythonChallenges.py
@@ -1,1 +1,19 @@
-print("Helloz");
+
+if __name__ == "__main__":
+    while(True):
+        print("-----------------------------------------------")
+        print("1. Array Sequences")
+        print("2. Stacks Queues & Dequeues")
+        print("3. Linked Lists")
+        print("4. Recursion")
+        print("5. Trees")
+        print("6. Searching and Sorting")
+        print("7. Riddles")
+        print("------------------------------------------------")
+        res = input("Please enter your response: ")
+        if(res == "1"):
+            print("Arrays")
+        res = input("Do you want to continue : ")
+        if(res != "y"):
+            break
+
